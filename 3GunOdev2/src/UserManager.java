229270def71b1208b425bd9325357ce77c98958b
Ksbasyigit <ksbasyigit@gmail.com>
2021-05-03 23:34:36 +0300

    3GunOdev2 Commit

---
+++ 3GunOdev2/src/UserManager.java
@@ -0,0 +1,14 @@
+
+public class UserManager {
+	public void add() {
+		System.out.println("User Ekleme");
+	}
+
+	public void del() {
+		System.out.println("User Silme");
+	}
+
+	public void update() {
+		System.out.println("User Güncelle");
+	}
+}
